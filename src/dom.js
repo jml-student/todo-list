1f21193fb1f226d4baa46c7c64af3506bf1b38b5
jml-student <student741@proton.me>
2024-06-28 19:41:01 +0200

    Add item buttons, their styles and item open-close logic

--- src/dom.js
+++ src/dom.js
@@ -55,23 +55,51 @@ export function displayContent(Projects, project) {
         itemPrio.textContent = item.priority;
 
         let itemStatusDiv = document.createElement('div');
+        itemStatusDiv.classList.add('item-status-div');
         let itemStatusFirst = document.createElement('div');
+        itemStatusFirst.classList.add('item-status-first');
         let itemStatusSecond = document.createElement('div');
+        itemStatusSecond.classList.add('item-status-second');
 
         let itemButtonsDiv = document.createElement('div');
         itemButtonsDiv.classList.add('item-buttons-div');
 
         let itemEditButton = document.createElement('button');
+        itemEditButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z" /></svg>';
         itemEditButton.classList.add('item-edit-button');
 
         let itemDeleteButton = document.createElement('button');
+        itemDeleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>';
         itemDeleteButton.classList.add('item-delete-button');
 
-        let itemArrowDiv = document.createElement('div');
-        itemArrowDiv.classList.add('item-arrow-div');
-
         let itemArrowButton = document.createElement('button');
+        itemArrowButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>';
         itemArrowButton.classList.add('item-arrow-button');
+        
+        let arrowButtonStatus = 'close';
+
+        itemArrowButton.addEventListener('click', () => {
+            if (arrowButtonStatus === 'close') {
+                itemDes.style.display = 'block';
+                itemPrio.style.display = 'block';
+                itemStatusDiv.style.display = 'block';
+                itemButtonsDiv.style.display = 'block';
+
+                itemArrowButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" /></svg>';
+
+                arrowButtonStatus = 'open';
+
+            } else if (arrowButtonStatus === 'open') {
+                itemDes.style.display = 'none';
+                itemPrio.style.display = 'none';
+                itemStatusDiv.style.display = 'none';
+                itemButtonsDiv.style.display = 'none';
+
+                itemArrowButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>';
+
+                arrowButtonStatus = 'close';
+            }
+        })
 
         itemStatusDiv.appendChild(itemStatusFirst);
         itemStatusDiv.appendChild(itemStatusSecond);
@@ -79,20 +107,19 @@ export function displayContent(Projects, project) {
         itemButtonsDiv.appendChild(itemEditButton);
         itemButtonsDiv.appendChild(itemDeleteButton);
 
-        itemArrowDiv.appendChild(itemArrowButton);
-
         itemContainer.appendChild(itemTitle);
-        itemContainer.appendChild(itemDes);
         itemContainer.appendChild(itemDate);
+        itemContainer.appendChild(itemDes);
         itemContainer.appendChild(itemPrio);
 
         itemContainer.appendChild(itemStatusDiv);
         itemContainer.appendChild(itemButtonsDiv);
-        itemContainer.appendChild(itemArrowDiv);
+
+        itemContainer.appendChild(itemArrowButton);
 
         content.appendChild(itemContainer);
     });
-    
+
     const addItemDiv = document.createElement('div');
     addItemDiv.classList.add('button-container');
     const addItemButton = document.createElement('button');
